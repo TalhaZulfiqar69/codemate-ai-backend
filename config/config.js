3fab205c8dc5a0a0745a1c2e255b64f0224e9b7f
Muhammad Talha Zulfiqar <apple@Muhammad-Talha-Zulfiqar.local>
2026-05-17 16:48:31 +0200

    chores: vulnerabilties fix

--- config/config.js
+++ config/config.js
@@ -1,29 +1,29 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const config = {
+export default {
   development: {
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || null,
-    database: process.env.DATABASE_DATABASE || 'database_test',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    dialect: process.env.DATABASE_DIALECT || 'postgres',
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT || 5432,
   },
   test: {
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD || null,
-    database: process.env.DATABASE_DATABASE || 'database_test',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    dialect: process.env.DATABASE_DIALECT || 'mysql',
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT || 5432,
   },
   production: {
-    username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD || null,
-    database: process.env.DATABASE_DATABASE || 'database_production',
-    host: process.env.DATABASE_HOST || '127.0.0.1',
-    dialect: process.env.DATABASE_DIALECT || 'mysql',
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    port: process.env.DATABASE_PORT || 5432,
   }
 };
-
-export default config;
